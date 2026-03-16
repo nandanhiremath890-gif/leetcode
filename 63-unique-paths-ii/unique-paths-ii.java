878a20e1f37b82ef7dc5546d03df7c51d1d3957c
NandanHiremath <nandanhiremath890@gmail.com>
2026-03-16 21:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (94.97%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        // Start cell
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        dp[0][0] = 1;
+
+        // First column
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                dp[i][0] = dp[i - 1][0];
+            } else {
+                dp[i][0] = 0;
+            }
+        }
+
+        // First row
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 0) {
+                dp[0][j] = dp[0][j - 1];
+            } else {
+                dp[0][j] = 0;
+            }
+        }
+
+        // Fill remaining grid
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
