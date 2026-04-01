f81927165c872dc7a683c6800975d4c27758ae5b
NandanHiremath <nandanhiremath890@gmail.com>
2026-04-01 20:45:38 +0530

    Time: 8 ms (8.47%) | Memory: 53.5 MB (80.93%) - LeetSync

---
+++ 89-gray-code/gray-code.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+
+        for (int i = 0; i < n; i++) {
+            int size = result.size();
+            // Reflect and add
+            for (int j = size - 1; j >= 0; j--) {
+                result.add(result.get(j) | (1 << i));
+            }
+        }
+
+        return result;
+    }
+}
