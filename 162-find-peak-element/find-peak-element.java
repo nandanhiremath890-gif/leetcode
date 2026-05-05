57a94530004ab843710408e6753e094ee0e1dadf
NandanHiremath <nandanhiremath890@gmail.com>
2026-05-05 18:18:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (73.78%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid; // peak is on left side
+            } else {
+                left = mid + 1; // peak is on right side
+            }
+        }
+
+        return left; // or right (both are same)
+    }
+}
